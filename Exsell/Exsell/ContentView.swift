289d6e26c20d978c56846f321b81fdb1ad01b962
Prince Wang <princewang0201@berkeley.edu>
2019-11-30 14:56:45 -0800

    "setup for yuki to pull"

---
+++ Exsell/Exsell/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Exsell
+//
+//  Created by Prince Wang on 2019/11/30.
+//  Copyright © 2019 MachineLearning@Blackwell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
